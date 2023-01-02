b97773bf851b858bb3a7d0fe758859bb7e107dcf
dzikrisee <marlianawatiazzahra@gmail.com>
2023-01-02 17:49:53 +0700

    edit js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ function opentab(tabname) {
   event.currentTarget.classList.add('active-link');
   document.getElementById(tabname).classList.add('active-tab');
 }
+// end tab open
 
 // side menu
 var sidemenu = document.getElementById('sidemenu');
@@ -20,3 +21,16 @@ function openmenu() {
 function closemenu() {
   sidemenu.style.right = '-200px';
 }
+// end side menu
+
+// Speedsheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzh6cdHj6vdw2J2RKkRMzPp__gmMCxs-ylV_KJI1eq3u8T911YWDoMjVMykaAxa_rAi/exec';
+const form = document.forms['Submit-From-to-Google-Sheets'];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => console.log('Success!', response))
+    .catch((error) => console.error('Error!', error.message));
+});
+// end Speedsheets
